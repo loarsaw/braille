2b843c74066be2a399566dce63cd0f6d48aa25f8
loarsaw <amanahmed1385@gmail.com>
2024-10-15 12:32:10 +0530

    bismillah

---
+++ src/components/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+const App = () => {
+  return <div className="h-screen w-screen flex items-center justify-center text-blue-600 text-2xl">App</div>;
+};
+
+export default App;
